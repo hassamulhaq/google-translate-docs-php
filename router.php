a5fb6216da09dc348c4a3bbc6b2368401a7b7cc0
hassamulhaq <developer.hassam@gmail.com>
2024-08-30 16:23:53 +0500

    Secure service-account.json file, update rule for htaccess

--- router.php
+++ router.php
@@ -1,5 +1,12 @@
 <?php
 
+use Dotenv\Dotenv;
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Define the routes
 $routes = [
     '/' => 'index.php',
@@ -17,13 +24,21 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Define the file extensions that should bypass routing
-$staticFileExtensions = ['css', 'js', 'png', 'jpg', 'jpeg', 'gif', 'svg', 'ico', 'woff', 'woff2', 'ttf', 'eot', 'otf', 'json', 'xml', 'txt', 'pdf', 'doc', 'docs', 'docx', 'pdf', 'csv', 'jpg', 'png', 'jpeg', 'gif', 'css', 'js', 'html', 'socs'];
+$staticFileExtensions = ['css', 'js', 'png', 'jpg', 'jpeg', 'gif', 'svg', 'ico', 'woff', 'woff2', 'ttf', 'eot', 'otf', 'json', 'xml', 'txt', 'pdf', 'doc', 'docs', 'docx', 'csv', 'html'];
 $fileExtension = pathinfo($path, PATHINFO_EXTENSION);
 // Check if the requested URL has a static file extension
 if (in_array($fileExtension, $staticFileExtensions)) {
     // Serve the file directly
     $staticFilePath = __DIR__ . $path;
     if (file_exists($staticFilePath) && is_file($staticFilePath)) {
+        // Prevent serving certain sensitive files directly
+        if (basename($staticFilePath) === $_ENV['GOOGLE_APPLICATION_CREDENTIALS'] ?? 'service-account.json') {
+            http_response_code(403);
+            echo "403 - Forbidden";
+            exit;
+        }
+
+        // Serve the file with the correct content type
         header('Content-Type: ' . mime_content_type($staticFilePath));
         readfile($staticFilePath);
     } else {
